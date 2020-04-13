013b50a6772e3a478edfc0956de53243876133b9
Crowdbotics <team@crowdbotics.com>
2020-04-13 02:29:48 +0000

    Update models

--- backend/thrive_boutique_co_15782/wsgi.py
+++ backend/thrive_boutique_co_15782/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thrive_boutique_co_15782.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thrive_boutique_co_15782.settings")
 
 application = get_wsgi_application()
